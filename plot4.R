c5f40d3367635514d5aa688f71d27a16b0394912
LilaLipetti <dj.romy.fi@gmail.com>
2015-03-03 15:10:27 +0200

    plot4 fixing
    plot 4 fixing

--- plot4.R
+++ plot4.R
@@ -37,11 +37,11 @@ plot(subData$DateAndTime,subData$Voltage,type="l",xlab="datetime",ylab="Voltage"
 plot(subData$DateAndTime,subData$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
 lines(subData$DateAndTime,subData$Sub_metering_2,type="l",col="red")
 lines(subData$DateAndTime,subData$Sub_metering_3,type="l",col="blue")
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=1,col=c("black","red","blue"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=1,col=c("black","red","blue"),bty="n")
 
 
 ## bottom right: Generating line grap for Global Reactive Power
-plot(subData$DateAndTime,subData$Global_active_power,type="l",xlab="datetime",ylab="Global_active_power")
+plot(subData$DateAndTime,subData$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
 
 
 ##closing the file
